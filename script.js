00a782680fabbc4ff29cf951eab0ace13f940462
PR7175Z <aadimpraz@gmail.com>
2024-09-07 09:36:20 +0545

    Event handler for value change

--- script.js
+++ script.js
@@ -12,56 +12,61 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function chainreaction(e){
+        const currentClass = e.target.classList;
+        const currentParent = e.target.parentNode.classList;
+
+        const currentCol = currentClass.value.split('column')[1];
+        const currentRowCount = currentParent.value.split('row')[1];
+        const uprow = parseInt(currentRowCount) - 1;
+        const downrow = parseInt(currentRowCount) + 1; 
+        const leftcol = parseInt(currentCol) - 1;
+        const rightcol = parseInt(currentCol) + 1;
+
+        setTimeout(() => {
+            if(document.querySelector(`.row${uprow} .column${currentCol}`)){
+                document.querySelector(`.row${uprow} .column${currentCol}`).innerHTML = parseInt(document.querySelector(`.row${uprow} .column${currentCol}`).innerHTML) + 1;
+            }if(document.querySelector(".row"+downrow+" .column"+currentCol)){
+                document.querySelector(".row"+downrow+" .column"+currentCol).innerHTML = parseInt(document.querySelector(".row"+downrow+" .column"+currentCol).innerHTML) + 1;
+            }if(document.querySelector(".row"+currentRowCount+" .column"+leftcol)){
+                document.querySelector(".row"+currentRowCount+" .column"+leftcol).innerHTML = parseInt(document.querySelector(".row"+currentRowCount+" .column"+leftcol).innerHTML) + 1;
+            }if(document.querySelector(".row"+currentRowCount+" .column"+rightcol)){    
+                document.querySelector(".row"+currentRowCount+" .column"+rightcol).innerHTML = parseInt(document.querySelector(".row"+currentRowCount+" .column"+rightcol).innerHTML) + 1;
+            }
+        }, 500);
+    }
+
     let p =document.querySelectorAll('p');
     p.forEach((element) => {
         element.addEventListener('click', (e)=>{
-            let val = e.target.getAttribute('data-value');
+            // let val = e.target.getAttribute('data-value');
+            let val = parseInt(e.target.innerHTML);
             val = (val == null)? 0: val;
             val++;
             if(val < 4){
                 e.target.innerHTML = val;
                 e.target.setAttribute('data-value', val);
-                let currentClass = e.target.classList;
-                let currentParent = e.target.parentNode.classList;
-
-                let columnCount = currentClass.value.split('column');
-                // console.log(columnCount[1]);
-
-                // console.log(currentClass);
-                // console.log(currentParent);
             }else{
-                e.target.innerHTML = 0;
-                e.target.setAttribute('data-value', 0);
-                let currentClass = e.target.classList;
-                let currentParent = e.target.parentNode.classList;
-
-                const columnCount = currentClass.value.split('column');
-                const rowCount = currentParent.value.split('row');
-                const currentCol = columnCount[1];
-                const currentRowCount = rowCount[1];
-                const uprow = parseInt(currentRowCount) - 1;
-                const downrow = parseInt(currentRowCount) + 1; 
-                const leftcol = parseInt(currentCol) - 1;
-                const rightcol = parseInt(currentCol) + 1;
-
-                document.querySelector(".row"+uprow+" .column"+currentCol).innerHTML = parseInt(document.querySelector(".row"+uprow+" .column"+currentCol).innerHTML) + 1;
-                document.querySelector(".row"+downrow+" .column"+currentCol).innerHTML = parseInt(document.querySelector(".row"+downrow+" .column"+currentCol).innerHTML) + 1;
-                document.querySelector(".row"+currentRowCount+" .column"+leftcol).innerHTML = parseInt(document.querySelector(".row"+currentRowCount+" .column"+leftcol).innerHTML) + 1;
-                document.querySelector(".row"+currentRowCount+" .column"+rightcol).innerHTML = parseInt(document.querySelector(".row"+currentRowCount+" .column"+rightcol).innerHTML) + 1;
-
-                // console.log(leftcol);
-                // console.log(rightcol);
-                // console.log(uprow);
-                // console.log(rightrow);
-
-                // console.log(currentClass);
-                // console.log(currentParent);
-                
+                e.target.innerHTML = val;
+                setTimeout(() => {
+                    e.target.innerHTML = 0;
+                    e.target.setAttribute('data-value', 0);
+                }, 500);
             }
         });
 
-        element.addEventListener('change', (e) => {
-            console.log('check')
-        })
+        observer = new MutationObserver((mutationsList, observer) => {
+            const parent = mutationsList[0].target.parentNode;
+            const currentNodeValue = mutationsList[0].target.innerHTML;
+            // console.log(parent);
+            if(currentNodeValue >= 4){
+                setTimeout(() => {
+                    mutationsList[0].target.innerHTML = 0;
+                }, 500);
+                chainreaction(mutationsList[0]);
+            }
+        });
+
+        observer.observe(element, {characterData: false, childList: true, attributes: false});
     })
 });
